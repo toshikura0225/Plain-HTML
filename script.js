86e1119ee20b1a7a6755b1d2f67fea74fc0ece81
toshikura0225 <t_kuramata@yahoo.co.jp>
2016-09-22 18:51:26 +0900

    for node

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-﻿$(function () {
+﻿var socket;
+
+$(function () {
+	
+	// サーバーに接続
+	socket = io.connect();
+	
+	// メッセージ受信イベント（PVを受信）
+	socket.on('path-through', function (data) {
+		
+		$('#idReceived').prepend(data + '<br/>');		// デバッグ用に書き出し
+		
+	});	
 	
 });
+
+function debug1() {
+	socket.emit('path-through', 'test message');
+}
